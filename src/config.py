76ff54c250fc38f90cc086520f08292d14bb55a7
Sergey <badrizlovsergey@gmail.com>
2025-11-22 23:26:12 +0600

    fix(config): add verification environment variables

--- src/config.py
+++ src/config.py
@@ -1,7 +1,23 @@
 import os
+import sys
+import logging
 from dotenv import load_dotenv
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger(__name__)
 
 load_dotenv()
 
 TOKEN = os.getenv("TG_BOT_API_TOKEN")
 REFERENCE_BOOK_FILE_PATH = os.getenv("REFERENCE_BOOK_FILE_PATH")
+
+if not TOKEN:
+    logger.error("ОШИБКА: Не задана переменная окружения TG_BOT_API_TOKEN")
+    sys.exit(1)
+
+if not REFERENCE_BOOK_FILE_PATH:
+    logger.error("ОШИБКА: Не задана переменная окружения REFERENCE_BOOK_FILE_PATH")
+    sys.exit(1)
